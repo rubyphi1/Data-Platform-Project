38b55b612bdd95a7c683d3f5fc192b89bd752edf
rubyphi1 <rubyphi1007@gmail.com>
2023-05-25 16:30:35 +0930

    Adding function to drop NA rows and upload data to BQ

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM prefecthq/prefect:2.7.7-python3.9
 COPY docker-requirements.txt .
 
 RUN pip install -r docker-requirements.txt --trusted-host pypi.python.org --no-cache-dir
+Run pip install pandas-gbq
 
 COPY flows /opt/prefect/flows
-RUN mkdir -p /opt/prefect/data/yellow
-#COPY data  /opt/prefect/data
+#RUN mkdir -p /opt/prefect/data/yellow
+COPY data  /opt/prefect/data
--- flows/parameterized_flow.py
+++ flows/parameterized_flow.py
@@ -4,11 +4,14 @@ from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from random import randint
 import fastparquet
-
+from prefect_gcp import GcpCredentials
 
 @task(log_prints = True,retries=3)    
-def fetch(dataset_url: str) -> pd.DataFrame:
+def fetch_transform(dataset_url: str) -> pd.DataFrame:
     df = pd.read_parquet(dataset_url, engine='fastparquet')
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    df["passenger_count"].fillna(0, inplace=True)
+    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
     return df
 
 
@@ -27,16 +30,30 @@ def write_gcs(path) -> None:
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
+@task()
+def write_bq(df: pd.DataFrame) -> None:
+    """Write DataFrame to BiqQuery"""
+
+    gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+
+    df.to_gbq(
+        destination_table="data_project.taxi_rides",
+        project_id="utopian-outlet-384405",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append",
+    )
 
 @flow()
-def etl_web_to_gcs(month : int,year : int,  color : str) -> None:
+def etl_web_to_gcs_to_bq(month : int,year : int,  color : str) -> None:
     """The main ETL function"""
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url =f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}.parquet"
 
-    df = fetch(dataset_url)
+    df = fetch_transform(dataset_url)
     path = write_local(df, color, dataset_file)
     write_gcs(path)
+    write_bq(df)
  
     
 @flow()
@@ -44,7 +61,7 @@ def etl_parent_flow(
     months: list[int] = [4,5], year: int = 2022, color: str = "yellow"
 ):
     for month in months:
-        etl_web_to_gcs(month,year, color)
+        etl_web_to_gcs_to_bq(month,year, color)
 
 if __name__ == "__main__":
     color = "yellow"
