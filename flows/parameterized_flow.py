eba5a9c19c5d4737b3b2f9601331c91a7877cb45
rubyphi1 <rubyphi1007@gmail.com>
2023-05-30 21:52:09 +0930

    Loading parquet file from gcs direct to bq

--- flows/parameterized_flow.py
+++ flows/parameterized_flow.py
@@ -6,8 +6,13 @@ from prefect_gcp.cloud_storage import GcsBucket
 from random import randint
 import fastparquet
 from prefect_gcp import GcpCredentials
+import os
+from prefect.tasks import task_input_hash
+from time import time
+from datetime import timedelta
+from google.cloud import bigquery
 
-@task(log_prints = True,retries=3)    
+@task(log_prints = True)#,retries=3, cache_key_fn =task_input_hash,cache_expiration = timedelta(days=1))    
 def fetch_transform(dataset_url: str) -> pd.DataFrame:
     df = pd.read_parquet(dataset_url, engine='fastparquet')
     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
@@ -20,38 +25,42 @@ def merge_table_id(latest_id:int,df:pd.DataFrame) -> pd.DataFrame:
     a_shape = (df.shape[0], 1)
     fill_value = latest_id
     id = np.full(a_shape, int(fill_value))
-    id_df = pd.DataFrame(id, columns= ['ID'])
+    id_df = pd.DataFrame(id, columns= ['DataID'])
     merge_df = pd.concat([id_df, df], axis=1)
     return merge_df
 
-@task(log_prints = True)
-def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+@task(log_prints = True)#, cache_key_fn =task_input_hash,cache_expiration = timedelta(days=1))
+def write_local(merge_df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
     path = f"data/{color}/{dataset_file}"
-    df.to_parquet(path, compression="gzip")
+    merge_df.to_parquet(path, compression="gzip")
+    # # Checking where merge df is saved
+    #print("File location using os.getcwd():", os.getcwd())
     return path
 
 
-@task(log_prints = True)
+@task(log_prints = True, retries =3)#,cache_key_fn =task_input_hash,cache_expiration = timedelta(days=1))
 def write_gcs(path) -> None:
     """Upload local parquet file to GCS"""
     gcs_block = GcsBucket.load("gcs-bucket")
     gcs_block.upload_from_path(from_path=path, to_path=path)
-    return
+    return gcs_block
 
 @task()
-def write_bq(df: pd.DataFrame) -> None:
+def write_bq(gcs_block, color: str, dataset_file: str) -> None:
     """Write DataFrame to BiqQuery"""
-
     gcp_credentials_block = GcpCredentials.load("zoom-gcp-creds")
+    client = bigquery.Client(credentials=gcp_credentials_block.get_credentials_from_service_account())
+    table_id = "utopian-outlet-384405.data_project.taxi_rides"
+    bucket_name = "taxi_ny"
+    path = f"data/{color}/{dataset_file}"
+    
+    job_config = bigquery.LoadJobConfig(
+    source_format=bigquery.SourceFormat.PARQUET,)
 
-    df.to_gbq(
-        destination_table="data_project.taxi_rides",
-        project_id="utopian-outlet-384405",
-        credentials=gcp_credentials_block.get_credentials_from_service_account(),
-        chunksize=500_000,
-        if_exists="append",
-    )
+    uri = f"gs://{bucket_name}/{path}"
+    load_job = client.load_table_from_uri( uri, table_id, job_config=job_config)
+
 
 @flow()
 def etl_web_to_gcs_to_bq(month : int,year : int,  color : str) -> None:
@@ -66,16 +75,17 @@ def etl_web_to_gcs_to_bq(month : int,year : int,  color : str) -> None:
     merge_df = merge_table_id(latest_id,df)
     
     path = write_local(merge_df, color, dataset_file)
-    write_gcs(path)
-    write_bq(df)
+    gcs_block =  write_gcs(path)
+    write_bq(gcs_block,color, dataset_file)
  
     
 @flow()
 def etl_parent_flow(
-    months: list[int] = [4,5], year: int = 2022, color: str = "yellow"
+    months: list[int] = [4], year: int = 2022, color: str = "yellow"
 ):
     for month in months:
         etl_web_to_gcs_to_bq(month,year, color)
+        
 
 if __name__ == "__main__":
     color = "yellow"
