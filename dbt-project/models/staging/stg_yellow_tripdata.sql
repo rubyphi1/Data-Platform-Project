8f9e7ee4d5a88afb83e79e6d77af556b6b2c5913
rubyphi1 <rubyphi1007@gmail.com>
2023-06-07 18:34:19 +0930

    Adding function to drop duplicate row

--- dbt-project/models/staging/stg_yellow_tripdata.sql
+++ dbt-project/models/staging/stg_yellow_tripdata.sql
@@ -39,8 +39,9 @@ select
     cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
     cast(congestion_surcharge as numeric) as congestion_surcharge
-from {{source('staging','yellow_tripdata')}}
-where vendorid is not null
+from tripdata
+where rn = 1
+
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var('is_test_run', default=true) %}
