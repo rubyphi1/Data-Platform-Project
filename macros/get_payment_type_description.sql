a5033e3f2cc50df920c983fb97c686ec6ff6c975
Ruby Phi <rubyphi1007@gmail.com>
2023-06-12 12:38:12 +0000

    Fixing payment accept values

--- macros/get_payment_type_description.sql
+++ macros/get_payment_type_description.sql
@@ -5,6 +5,7 @@
 {% macro get_payment_type_description(payment_type) -%}
 
     case {{ payment_type }}
+        when 0 then null
         when 1 then 'Credit card'
         when 2 then 'Cash'
         when 3 then 'No charge'
